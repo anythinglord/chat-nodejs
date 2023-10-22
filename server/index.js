e0af1a5fb243e39d1c013d2244203ecf5f27fffd
anythinglord <sjsalcedoa@unal.edu.co>
2023-10-21 21:45:29 -0500

    create a server with express, show the index.html

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import logger from 'morgan'
+
+const port = process.env.PORT ?? 3000
+const app = express()
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd()+'/client/index.html')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
