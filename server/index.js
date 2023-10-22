805b1bd746782858ba4c6db51deefbecd3a50cc2
anythinglord <sjsalcedoa@unal.edu.co>
2023-10-21 22:17:11 -0500

    create a web socket server and use client to send a message

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 import express from "express";
 import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const port = process.env.PORT ?? 3000
 const app = express()
-app.use(logger('dev'))
 
+// http server
+const server = createServer(app)
+
+// Web sockets server
+const io = new Server(server)
+io.on('connection', (socket) => {
+    console.log('user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        console.log('message: '+ msg)
+    })
+})
+
+app.use(logger('dev'))
 app.get('/', (req, res) => {
     res.sendFile(process.cwd()+'/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
 
